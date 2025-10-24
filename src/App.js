b40231d3668e766c71af1390dfc95afb180670ec
Surgithan <surgithan4645@gmail.com>
2025-10-24 18:29:00 +0530

    commit changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Components/Nav';
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <h1>Push project to react dev</h1>
+    </div>
+  )
+}
+
+export default App;
