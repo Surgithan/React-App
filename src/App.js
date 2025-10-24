9a368da5b7a10aec12ac0d9d005d828f8271024d
Surgithan <surgithan4645@gmail.com>
2025-10-24 18:21:14 +0530

    Resolved merge conflict while pulling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <div>
-      
+      <Nav/>
+      <h1>Push project to origin</h1>
     </div>
   );
 }
